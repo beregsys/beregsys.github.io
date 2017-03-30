c3cd794dd4200ea9bd5c921bd77d98fd221fd7cc
Sviatoslav <beregsys@gmail.com>
2017-03-30 14:56:13 +0300

    Auto-committing 'main.js'

--- gsm/js/main.js
+++ gsm/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 		event.preventDefault();
 		var id = $(this).prev().attr("id"),
 			tel = $(this).prev(),
-			fullTel = (id == "free-tel") : "888" ? "999";
+			fullTel = (id == "free-tel") ? "888" : "999";
 		tel.text(fullTel);
 	});
 });
