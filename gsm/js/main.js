00da23da458dfe69c77ec95616261ed610fe6d38
Sviatoslav <beregsys@gmail.com>
2017-04-03 19:20:50 +0300

    Auto-committing 'main.js'

--- gsm/js/main.js
+++ gsm/js/main.js
@@ -28,4 +28,20 @@ $(document).ready(function(){
         }
 
     });
+    $('.popup-with-form').magnificPopup({
+        type: 'inline',
+        preloader: false,
+        focus: '#name',
+        mainClass: 'mfp-fade',
+        removalDelay: 300,
+        callbacks: {
+            beforeOpen: function() {
+                if($(window).width() < 700) {
+                    this.st.focus = false;
+                } else {
+                    this.st.focus = '#name';
+                }
+            }
+        }
+    });
 });
