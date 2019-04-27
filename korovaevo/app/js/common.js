5b3b9b1c64054045301d7ec3fa2af55a304f46b0
Sviatoslav <beregsys@gmail.com>
2019-04-27 12:23:15 +0300

    first section almost ready

--- korovaevo/app/js/common.js
+++ korovaevo/app/js/common.js
@@ -1,5 +1,34 @@
 $(function() {
+	$(document).ready(function(){
+		// $('.main-slider').owlCarousel({
+		// 	items   : 1,
+		// });
 
-	// Custom JS
+		var owl = $('.main-slider');
+		owl.owlCarousel({
+			items         : 1,
+			dotsContainer : '.slider-dots',
+			//dots          : false
+		});
 
+		$('.slider-dots__item').click(function(){
+			owl.trigger('to.owl.carousel', [$(this).index(), 300]);
+		});
+
+		// Go to the next item
+		$('.btn-next').click(function(e) {
+			e.preventDefault();
+			
+			owl.trigger('next.owl.carousel');
+		})
+	// Go to the previous item
+		$('.btn-prev').click(function(e) {
+			e.preventDefault();
+				owl.trigger('prev.owl.carousel', [300]);
+		});
+
+		
+
+
+	});
 });
